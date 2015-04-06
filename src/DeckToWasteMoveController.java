d90e43cae3b3fa73f9887f4193b9697993c152e6
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-04-06 16:38:55 +0200

    Update DTWMCT (@before sizeFoundations) 	Update DTWMC (método sizeFoundations)

--- src/DeckToWasteMoveController.java
+++ src/DeckToWasteMoveController.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
+
 
 public class DeckToWasteMoveController {
 
 	public int sizeWaste() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	public ArrayList<Integer> sizeFoundations() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/DeckToWasteMoveControllerTest.java
+++ src/DeckToWasteMoveControllerTest.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import org.junit.Before;
 
 public class DeckToWasteMoveControllerTest {
@@ -5,11 +7,13 @@ public class DeckToWasteMoveControllerTest {
 	private DeckToWasteMoveController deckToWasteMoveController;
 	
 	private int sizeWaste;
+	private ArrayList<Integer> sizeFoundations;
 	
 	@Before
 	public void before() {
 		deckToWasteMoveController = new DeckToWasteMoveController();
 		sizeWaste = deckToWasteMoveController.sizeWaste();
+		sizeFoundations = deckToWasteMoveController.sizeFoundations();
 	}
 
 }
