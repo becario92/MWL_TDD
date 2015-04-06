e4dbab18f4ba90231fe413f30bf50db5962239be
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-04-06 16:27:49 +0200

    Update DTWMCT (@before)

--- src/DeckToWasteMoveControllerTest.java
+++ src/DeckToWasteMoveControllerTest.java
@@ -1,6 +1,12 @@
+import org.junit.Before;
 
 public class DeckToWasteMoveControllerTest {
 	
 	private DeckToWasteMoveController deckToWasteMoveController;
+	
+	@Before
+	public void before() {
+		deckToWasteMoveController = new DeckToWasteMoveController();
+	}
 
 }
