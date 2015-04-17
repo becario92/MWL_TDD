836617b323e62860f0015c802c974f95299bbb3c
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-04-17 12:35:06 +0200

    Update WTFMCT (método before)

--- src/WasteToFoundationMoveControllerTest.java
+++ src/WasteToFoundationMoveControllerTest.java
@@ -1,6 +1,13 @@
+import org.junit.Before;
+
 
 public class WasteToFoundationMoveControllerTest {
 	
 	private WasteToFoundationMoveController wasteToFoundationMoveController;
+	
+	@Before
+	public void before() {
+		wasteToFoundationMoveController = new WasteToFoundationMoveController();
+	}
 
 }
