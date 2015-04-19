51dfa22a457257f9b75da4f7c57e0ae08805a8c6
becario92 <fjalva01@gmail.com>
2015-04-19 17:30:59 +0200

    Update GameController (singleton; con acceso a las pilas de cartas)

--- src/GameController.java
+++ src/GameController.java
@@ -1,4 +1,32 @@
+import java.util.HashMap;
+import java.util.Stack;
+
 
 public class GameController {
 
+	private static GameController GAMECONTROLLER;
+	
+	private Stack<Card> deckStack;
+	private Stack<Card> wasteStack;
+	private HashMap<Suit, Stack<Card>> foundationsStack;
+	
+	private GameController() {
+	}
+	
+	public static GameController getGameController() {
+		return GAMECONTROLLER;
+	}
+
+	protected Stack<Card> getDeckStack() {
+		return deckStack;
+	}
+
+	protected Stack<Card> getWasteStack() {
+		return wasteStack;
+	}
+
+	protected HashMap<Suit, Stack<Card>> getFoundationsStack() {
+		return foundationsStack;
+	}
+	
 }
