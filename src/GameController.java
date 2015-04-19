14d3be269b3a3f48c2d49a7a735d921420058083
becario92 <fjalva01@gmail.com>
2015-04-19 17:34:49 +0200

    Update GameController (inicialización de un estado inicial de las pilas para pasar los tests)

--- src/GameController.java
+++ src/GameController.java
@@ -4,13 +4,25 @@ import java.util.Stack;
 
 public class GameController {
 
-	private static GameController GAMECONTROLLER;
+	private static GameController GAMECONTROLLER = new GameController();
 	
 	private Stack<Card> deckStack;
 	private Stack<Card> wasteStack;
 	private HashMap<Suit, Stack<Card>> foundationsStack;
 	
 	private GameController() {
+		deckStack = new Stack<Card>();
+		deckStack.push(new Card(false, Suit.SPADES, 1));
+		
+		wasteStack = new Stack<Card>();
+		wasteStack.push(new Card(true, Suit.SPADES, 1));
+		
+		foundationsStack = new HashMap<Suit, Stack<Card>>();
+		for(Suit suit : Suit.values()) {
+			Stack<Card> foundationStack = new Stack<Card>();
+			foundationStack.push(new Card(true, suit, 0));
+			foundationsStack.put(suit, foundationStack);
+		}
 	}
 	
 	public static GameController getGameController() {
