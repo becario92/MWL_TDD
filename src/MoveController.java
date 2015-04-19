2f043506544c6c6cf5ba46c11dd1b3b8fdeb79cf
becario92 <fjalva01@gmail.com>
2015-04-19 17:48:44 +0200

    Delete GameController (no termina de tener sentido la clase porque startGameController tiene un estado inicial diferente; se vuelca todo en MoveController)

--- src/MoveController.java
+++ src/MoveController.java
@@ -1,4 +1,48 @@
+import java.util.HashMap;
+import java.util.Stack;
 
-public class MoveController {
 
+public abstract class MoveController {
+	
+	private Stack<Card> deckStack;
+	private Stack<Card> wasteStack;
+	private HashMap<Suit, Stack<Card>> foundationsStack;
+	
+	protected MoveController() {
+		deckStack = new Stack<Card>();
+		deckStack.push(new Card(false, Suit.SPADES, 1));
+		
+		wasteStack = new Stack<Card>();
+		wasteStack.push(new Card(true, Suit.SPADES, 1));
+		
+		foundationsStack = new HashMap<Suit, Stack<Card>>();
+		for(Suit suit : Suit.values()) {
+			Stack<Card> foundationStack = new Stack<Card>();
+			foundationStack.push(new Card(true, suit, 0));
+			foundationsStack.put(suit, foundationStack);
+		}
+	}
+
+	public Card move(Stack<Card> from, Stack<Card> to) {
+		if(canMove(from, to)) {
+			to.push(from.pop());
+			return to.peek();
+		}
+		return null;
+	}
+
+	protected abstract boolean canMove(Stack<Card> from, Stack<Card> to);
+	
+	protected Stack<Card> getDeckStack() {
+		return deckStack;
+	}
+
+	protected Stack<Card> getWasteStack() {
+		return wasteStack;
+	}
+
+	protected HashMap<Suit, Stack<Card>> getFoundationsStack() {
+		return foundationsStack;
+	}
+	
 }
